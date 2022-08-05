9a5b7841342ba12f69da3b94a848ba2332efec9d
sam-fernando <0122333sam@gmail.com>
2022-08-05 21:27:05 +0530

    nothing special

---
+++ app/src/main/java/com/example/callingteacher/RegisterActivity.java
@@ -0,0 +1,129 @@
+package com.example.callingteacher;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextInputEditText etName;
+    TextInputEditText etRegEmail;
+    TextInputEditText etRegPassword;
+    TextView tvLoginHere;
+    Button btnRegister;
+    ProgressBar progressBar;
+
+    FirebaseAuth mAuth;
+
+    FirebaseUser fbUser;
+    DatabaseReference dbRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etName = findViewById(R.id.etName);
+        etRegEmail = findViewById(R.id.etRegEmail);
+        etRegPassword = findViewById(R.id.etRegPass);
+        tvLoginHere = findViewById(R.id.tvLoginHere);
+        btnRegister = findViewById(R.id.btnRegister);
+        progressBar = findViewById(R.id.pbar);
+
+        mAuth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        btnRegister.setOnClickListener(view ->{
+            createUser();
+        });
+
+        tvLoginHere.setOnClickListener(view ->{
+            startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+        });
+    }
+
+    private void createUser(){
+        String email = etRegEmail.getText().toString();
+        String password = etRegPassword.getText().toString();
+        String name = etName.getText().toString();
+
+        if (TextUtils.isEmpty(email)){
+            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.requestFocus();
+        }else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Password cannot be empty");
+            etRegPassword.requestFocus();
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "User registered successfully", Toast.LENGTH_SHORT).show();
+
+
+                        fbUser = mAuth.getCurrentUser();
+                        dbRef = dbRef.child(fbUser.getUid());
+
+                        HashMap<String,String> hashmap = new HashMap<>();
+                        hashmap.put("id",fbUser.getUid());
+                        hashmap.put("name",name);
+                        hashmap.put("email",email);
+                        hashmap.put("password",password);
+                        hashmap.put("imageURL", "default");
+                        hashmap.put("status","offline");
+                        hashmap.put("search",name.toLowerCase());
+
+
+                        dbRef.setValue(hashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+
+                                progressBar.setVisibility(View.GONE);
+                                if(task.isSuccessful())
+                                {
+
+                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                                else
+                                {
+
+                                    Toast.makeText(RegisterActivity.this, "Could not create USER", Toast.LENGTH_SHORT).show();
+
+                                }
+
+                            }
+                        });
+
+
+                        //startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+                    }else{
+                        Toast.makeText(RegisterActivity.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+}
